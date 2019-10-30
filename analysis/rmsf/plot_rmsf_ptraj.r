417fb7cc93d042a85d692a8ceab302598f5fb465
Xin-Qiu Yao <xinqiu.yao@gmail.com>
2019-10-30 17:16:59 -0400

    ENHANCEMENT: add differential RMSF plot

--- analysis/rmsf/plot_rmsf_ptraj.r
+++ analysis/rmsf/plot_rmsf_ptraj.r
@@ -16,7 +16,7 @@ rr2 <- read.table('rms2.out')
 
 rrs <- rbind(rr[, 2], rr2[, 2])
 pdf(onefile=TRUE, file='rmsf.pdf', width=3.8, height=3.2)
-plot.fluct(rrs, col=c('black', 'red'), lab=c('WT', 'Mutant'), 
+plot.fluct(rrs, col=c('black', 'red'), lab=c('WT', 'Mutant'), ylim2zero=FALSE,
    sse=sse, typ='l', sse.min.length=3, ylab="RMSF (A)", axes=FALSE)
 axis(2)
 box()
@@ -31,3 +31,25 @@ dev.off()
 ## write b factors
 write.pdb(pdb, b=vec2resno(rrs[1, ], pdb$atom$resno), file="rmsf1.pdb")
 write.pdb(pdb, b=vec2resno(rrs[2, ], pdb$atom$resno), file="rmsf2.pdb")
+
+
+## differential rmsf
+#drrs <- rbind(rr2[, 2] - rr[, 2], rr3[, 2] - rr[, 2], rr4[, 2] - rr[, 2])
+drrs <- rbind(rr2[, 2] - rr[, 2])
+
+pdf(onefile=TRUE, file='drmsf.pdf', width=6, height=4.5)
+plot.fluct(drrs, col=c('red'), lab=c('Mutant - WT'), ylim2zero=FALSE,
+   sse=sse, typ='l', sse.min.length=3, ylab="DRMSF (A)", axes=FALSE)
+axis(2)
+box()
+res <- pdb$atom$resno[pdb$calpha]
+myres <- pretty(res, n=10)
+if(! res[1] %in% myres) myres[1] <- res[1]
+if(! res[length(res)] %in% myres) myres[length(myres)] <- res[length(res)]
+xticks <- match(myres, res)
+axis(1, at=xticks, labels=myres)
+dev.off()
+
+## write b factors
+write.pdb(pdb, b=abs(vec2resno(drrs[1, ], pdb$atom$resno)), o=vec2resno(drrs[1, ], pdb$atom$resno), file="drmsf.pdb")
+
